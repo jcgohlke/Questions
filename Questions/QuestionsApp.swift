be6bb8b1eb39ba390facd151af7df55e95bdb7eb
Ben Gohlke <ben@launchcontrol.ventures>
2023-02-20 09:31:34 -0500

    Initial Commit

---
+++ Questions/QuestionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionsApp.swift
+//  Questions
+//
+//  Created by Joben Gohlke on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct QuestionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
